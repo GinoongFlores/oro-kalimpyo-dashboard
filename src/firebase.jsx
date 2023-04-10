0e4b7c8dcd95ad1e0829cca0e8060b7f7521da8f
ginoongflores <florespaul161@gmail.com>
2023-04-10 20:29:51 +0800

    fix(firebase): append env using VITE

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-	authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_KEY,
+	authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: "https://orokalimpyo-default-rtdb.firebaseio.com",
 	projectId: "orokalimpyo",
 	storageBucket: "orokalimpyo.appspot.com",
 	messagingSenderId: "772158884703",
-	appId: "1:772158884703:web:1aa813ecd681fbad9c04ea",
-	measurementId: "G-MHMT4GFRF3",
+	appId: "1:772158884703:web:9a4cb8d12bffbd769c04ea",
+	measurementId: "G-8T329G2ZVR",
 };
 
 // Initialize Firebase
