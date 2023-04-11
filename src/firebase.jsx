9d7948c57fdbb4fe450ba47991fa9b2401ca6cdb
Christian Paul Flores <florespaul161@gmail.com>
2023-04-11 09:34:07 +0800

    update clenro-new-feature (branch) (#3)
    * temp(fix): firebase config
    
    * fix(firebase): append env using VITE
    
    * fix(firebase): append VITE on authDomain

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-	authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_KEY,
+	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: "https://orokalimpyo-default-rtdb.firebaseio.com",
 	projectId: "orokalimpyo",
 	storageBucket: "orokalimpyo.appspot.com",
 	messagingSenderId: "772158884703",
-	appId: "1:772158884703:web:c520e88c2a2a57569c04ea",
-	measurementId: "G-L10TR9T1BW",
+	appId: "1:772158884703:web:9a4cb8d12bffbd769c04ea",
+	measurementId: "G-8T329G2ZVR",
 };
 
 // Initialize Firebase
