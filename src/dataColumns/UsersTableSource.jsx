499b6810fabbd5792ce9fea7b48c22652500cebe
ginoongflores <florespaul161@gmail.com>
2023-04-15 16:59:44 +0800

    feat(userTableSource): add headerTheme

--- src/dataColumns/UsersTableSource.jsx
+++ src/dataColumns/UsersTableSource.jsx
@@ -1,17 +1,23 @@
 export const userColumns = [
 	{
+		headerClassName: "headerTheme",
+
 		field: "list",
+		headerClassName: "headerTheme",
 		headerName: "#",
 		width: 50,
 	},
 
 	// {
+
 	// 	field: "id",
 	// 	headerName: "ID",
 	// 	width: 100,
 	// },
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "name",
 		headerName: "Name",
 		width: 150,
@@ -27,36 +33,48 @@ export const userColumns = [
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "number",
 		headerName: "Number",
 		width: 120,
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "gender",
 		headerName: "Gender",
 		width: 100,
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "email",
 		headerName: "Email",
 		width: 200,
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "user_type",
 		headerName: "Type of HouseHold",
 		width: 200,
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "barangay",
 		headerName: "Barangay",
 		width: 150,
 	},
 
 	{
+		headerClassName: "headerTheme",
+
 		field: "address",
 		headerName: "Address",
 		width: 190,
