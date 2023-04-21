19f0fae65aefce35e11a250ddca0fb304c90821f
ginoongflores <florespaul161@gmail.com>
2023-04-21 21:00:26 +0800

    feat(barangayLists): separate a component to list all the barangay

---
+++ src/components/barangayLists/barangayLists.jsx
@@ -0,0 +1,34 @@
+export const barangayLists = [
+	{
+		value: "",
+		text: "Select a Barangay",
+	},
+	{
+		value: "AGUSAN",
+		text: "AGUSAN",
+	},
+	{
+		value: "BAIKINGON",
+		text: "BAIKINGON",
+	},
+	{
+		value: "BALUBAL",
+		text: "BALUBAL",
+	},
+	{
+		value: "BALULANG",
+		text: "BALULANG",
+	},
+	{
+		value: "BARANGAY 1",
+		text: "BARANGAY 1",
+	},
+	{
+		value: "BARANGAY 2",
+		text: "BARANGAY 2",
+	},
+	{
+		value: "BARANGAY 3",
+		text: "BARANGAY 3",
+	},
+];
