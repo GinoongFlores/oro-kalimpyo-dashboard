f0b12d418651cb88ec73051c0e72a66b0fd2aa7a
ginoongflores <florespaul161@gmail.com>
2023-04-24 20:38:02 +0800

    ref(BarangayList): rename text to label

--- src/components/barangayLists/BarangayLists.jsx
+++ src/components/barangayLists/BarangayLists.jsx
@@ -1,34 +1,323 @@
 export const BarangayLists = [
 	{
 		value: "",
-		text: "Select a Barangay",
+		label: "Select a Barangay",
 	},
 	{
 		value: "AGUSAN",
-		text: "AGUSAN",
+		label: "AGUSAN",
 	},
 	{
 		value: "BAIKINGON",
-		text: "BAIKINGON",
+		label: "BAIKINGON",
 	},
 	{
 		value: "BALUBAL",
-		text: "BALUBAL",
+		label: "BALUBAL",
 	},
 	{
 		value: "BALULANG",
-		text: "BALULANG",
+		label: "BALULANG",
 	},
 	{
 		value: "Barangay 1",
-		text: "Barangay 1",
+		label: "Barangay 1",
 	},
 	{
 		value: "Barangay 2",
-		text: "Barangay 2",
+		label: "Barangay 2",
 	},
 	{
 		value: "Barangay 3",
-		text: "Barangay 3",
+		label: "Barangay 3",
+	},
+
+	{
+		value: "Barangay 4",
+		label: "Barangay 4",
+	},
+	{
+		value: "Barangay 5",
+		label: "Barangay 5",
+	},
+	{
+		value: "Barangay 6",
+		label: "Barangay 6",
+	},
+	{
+		value: "Barangay 7",
+		label: "Barangay 7",
+	},
+	{
+		value: "Barangay 8",
+		label: "Barangay 8",
+	},
+	{
+		value: "Barangay 9",
+		label: "Barangay 9",
+	},
+	{
+		value: "Barangay 10",
+		label: "Barangay 10",
+	},
+	{
+		value: "Barangay 11",
+		label: "Barangay 11",
+	},
+	{
+		value: "Barangay 12",
+		label: "Barangay 12",
+	},
+	{
+		value: "Barangay 13",
+		label: "Barangay 13",
+	},
+	{
+		value: "Barangay 14",
+		label: "Barangay 14",
+	},
+	{
+		value: "Barangay 15",
+		label: "Barangay 15",
+	},
+	{
+		value: "Barangay 16",
+		label: "Barangay 16",
+	},
+	{
+		value: "Barangay 17",
+		label: "Barangay 17",
+	},
+	{
+		value: "Barangay 18",
+		label: "Barangay 18",
+	},
+	{
+		value: "Barangay 19",
+		label: "Barangay 19",
+	},
+	{
+		value: "Barangay 20",
+		label: "Barangay 20",
+	},
+	{
+		value: "Barangay 21",
+		label: "Barangay 21",
+	},
+	{
+		value: "Barangay 22",
+		label: "Barangay 22",
+	},
+	{
+		value: "Barangay 23",
+		label: "Barangay 23",
+	},
+	{
+		value: "Barangay 24",
+		label: "Barangay 24",
+	},
+	{
+		value: "Barangay 25",
+		label: "Barangay 25",
+	},
+	{
+		value: "Barangay 26",
+		label: "Barangay 26",
+	},
+	{
+		value: "Barangay 27",
+		label: "Barangay 27",
+	},
+	{
+		value: "Barangay 28",
+		label: "Barangay 28",
+	},
+	{
+		value: "Barangay 29",
+		label: "Barangay 29",
+	},
+	{
+		value: "Barangay 30",
+		label: "Barangay 30",
+	},
+	{
+		value: "Barangay 31",
+		label: "Barangay 31",
+	},
+	{
+		value: "Barangay 32",
+		label: "Barangay 32",
+	},
+	{
+		value: "Barangay 33",
+		label: "Barangay 33",
+	},
+	{
+		value: "Barangay 34",
+		label: "Barangay 34",
+	},
+	{
+		value: "Barangay 35",
+		label: "Barangay 35",
+	},
+	{
+		value: "Barangay 36",
+		label: "Barangay 36",
+	},
+	{
+		value: "Barangay 37",
+		label: "Barangay 37",
+	},
+	{
+		value: "Barangay 38",
+		label: "Barangay 38",
+	},
+	{
+		value: "Barangay 39",
+		label: "Barangay 39",
+	},
+	{
+		value: "Barangay 40",
+		label: "Barangay 40",
+	},
+	{
+		value: "Bayabas",
+		label: "Bayabas",
+	},
+	{
+		value: "Bayanga",
+		label: "Bayanga",
+	},
+	{
+		value: "Besigan",
+		label: "Besigan",
+	},
+	{
+		value: "Bonbon",
+		label: "Bonbon",
+	},
+	{
+		value: "Bugo",
+		label: "Bugo",
+	},
+	{
+		value: "Bulua",
+		label: "Bulua",
+	},
+	{
+		value: "Camaman-an",
+		label: "Camaman-an",
+	},
+	{
+		value: "Canito-an",
+		label: "Canito-an",
+	},
+	{
+		value: "Carmen",
+		label: "Carmen",
+	},
+	{
+		value: "Consolacion",
+		label: "Consolacion",
+	},
+	{
+		value: "Cugman",
+		label: "Cugman",
+	},
+	{
+		value: "Dansolihon",
+		label: "Dansolihon",
+	},
+	{
+		value: "F.S. Catanico",
+		label: "F.S. Catanico",
+	},
+	{
+		value: "Gusa",
+		label: "Gusa",
+	},
+	{
+		value: "Iponan",
+		label: "Iponan",
+	},
+	{
+		value: "Kauswagan",
+		label: "Kauswagan",
+	},
+	{
+		value: "Kimanait",
+		label: "Kimanait",
+	},
+	{
+		value: "Lapasan",
+		label: "Lapasan",
+	},
+	{
+		value: "Lumbia",
+		label: "Lumbia",
+	},
+	{
+		value: "Macao",
+		label: "Macao",
+	},
+	{
+		value: "Macasandig",
+		label: "Macasandig",
+	},
+	{
+		value: "Magsaysay",
+		label: "Magsaysay",
+	},
+	{
+		value: "Mandumol",
+		label: "Mandumol",
+	},
+	{
+		value: "Nazareth",
+		label: "Nazareth",
+	},
+	{
+		value: "Pagalungan",
+		label: "Pagalungan",
+	},
+	{
+		value: "Pagatpat",
+		label: "Pagatpat",
+	},
+	{
+		value: "Pigsag-an",
+		label: "Pigsag-an",
+	},
+	{
+		value: "Puerto",
+		label: "Puerto",
+	},
+	{
+		value: "Puntod",
+		label: "Puntod",
+	},
+	{
+		value: "San Simon",
+		label: "San Simon",
+	},
+	{
+		value: "Taglimao",
+		label: "Taglimao",
+	},
+	{
+		value: "Tagpangi",
+		label: "Tagpangi",
+	},
+	{
+		value: "Tignapoloan",
+		label: "Tignapoloan",
+	},
+	{
+		value: "Tuburan",
+		label: "Tuburan",
+	},
+	{
+		value: "Tumpagon",
+		label: "Tumpagon",
 	},
 ];
