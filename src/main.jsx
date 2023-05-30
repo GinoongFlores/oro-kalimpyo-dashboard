a830aea0a026e8893d9886346a3141131c97bb6c
ginoongflores <florespaul161@gmail.com>
2023-05-30 17:33:50 +0800

    feat(main): indention changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import UserAuthContext from "./context/UserAuthContext";
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-		<UserAuthContext>
-			<App />
-		</UserAuthContext>
+			<UserAuthContext>
+				<App />
+			</UserAuthContext>
 		</BrowserRouter>
 	</React.StrictMode>
 );
