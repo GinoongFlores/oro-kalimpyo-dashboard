4d095f5d2d1dc837bdde3f4bc9df10610c3613ee
GinoongFlores <florespaul161@gmail.com>
2023-09-12 21:51:30 +0800

    ref(DataTable): remove the array of `actionlColumn` and use `props`
    Use props to conditionally render each "actionColumn" based on each "DataGrid"

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -1,56 +1,7 @@
 import React from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { doc, deleteDoc } from "firebase/firestore";
-import { db } from "../../firebase";
-import ViewTableModal from "../modals/ViewTableModal";
 
-const DataTable = ({ props }) => {
-	const { rowData, columnData, isNeedAction, isDelete, isView } = props;
-	const actionColumn = [
-		{
-			field: "action",
-			headerName: "Action",
-			headerClassName: "headerTheme",
-			minWidth: 200,
-			flex: 1,
-			renderCell: (params) => {
-				return (
-					<>
-						<div className="cellAction">
-							{isView && <ViewTableModal />}
-							{isDelete && (
-								<button
-									onClick={() => {
-										handleDelete(params.row.id);
-									}}
-									className="deleteButton"
-								>
-									Delete
-								</button>
-							)}
-						</div>
-					</>
-				);
-			},
-		},
-	];
-
-	const handleDelete = async (id) => {
-		const confirmation = window.confirm(
-			"Are you sure you want to delete this admin?"
-		);
-
-		if (confirmation) {
-			await deleteDoc(doc(db, "Admins", id))
-				.then(() => {
-					console.log("Document successfully deleted!");
-				})
-				.catch((error) => {
-					console.error("Error removing document: ", error);
-				});
-		}
-	};
-
+const DataTable = ({ rowData, columnData, hasOwnAction, isOwnAction }) => {
 	return (
 		<div style={{ height: 600, width: "100%" }}>
 			<div style={{ display: "flex", height: "100%" }}>
@@ -62,9 +13,7 @@ const DataTable = ({ props }) => {
 								list: index + 1,
 							};
 						})}
-						columns={
-							isNeedAction ? columnData.concat(actionColumn) : columnData
-						}
+						columns={hasOwnAction ? columnData.concat(isOwnAction) : columnData}
 						pageSize={9}
 						density="comfortable"
 						rowsPerPageOptions={[9]}
