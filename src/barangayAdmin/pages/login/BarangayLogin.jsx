33707099921d06abde17de322c0e17ee21414bcc
GinoongFlores <florespaul161@gmail.com>
2023-10-23 20:09:23 +0800

    ref(BarangayLogin): optimize login functionality using `Formik`

--- src/barangayAdmin/pages/login/BarangayLogin.jsx
+++ src/barangayAdmin/pages/login/BarangayLogin.jsx
@@ -1,11 +1,12 @@
 // import "./login.scss";
 
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { toast } from "react-toastify";
-// import { useAuth } from "../../context/UserAuthContext";
+import { useAuthBarangay } from "../../context/BarangayAuthContext";
+import * as formik from "formik";
+import * as yup from "yup";
 
 // Logos
 import Logo from "../../../assets/oro-kalimpyo.png";
@@ -14,48 +15,15 @@ import SlDevs from "../../../assets/sldevs.png";
 import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
 import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
 
-const initialState = {
-	email: "",
-	password: "",
-};
-
 const BarangayLogin = () => {
-	// const { UserLogin } = useAuth();
-	const navigate = useNavigate();
-	const [error, setError] = useState(false);
+	const { UserLogin } = useAuthBarangay();
 	const [showPassword, setShowPassword] = useState(false);
-	const [user, setUser] = useState(initialState);
-	const { email, password } = user;
 
-	const handleLogin = async (e) => {
-		e.preventDefault();
-
-		if (email.length === 0 || password.length === 0) {
-			setError("");
-			toast.error("Please fill in all fields");
-		}
-		try {
-			await UserLogin(email, password);
-		} catch (error) {
-			if (error.code == "auth/user-not-found") {
-				setError("");
-				// toast.error("User not found");
-				return setError("User Not Found");
-			} else if (error.code == "auth/wrong-password") {
-				setError("");
-				// toast.error("Wrong password");
-				return setError("Wrong Password");
-			} else {
-				setError("");
-				return setError(`${error.message}`);
-			}
-		}
-	};
-
-	const handleInputChange = (e) => {
-		const { name, value } = e.target;
-		setUser({ ...user, [name]: value });
-	};
+	const { Formik } = formik;
+	const schema = yup.object().shape({
+		email: yup.string().email("invalid email").required("No Email Provided"),
+		password: yup.string().required("No password provided"),
+	});
 
 	const togglePasswordVisibility = () => {
 		setShowPassword(!showPassword);
@@ -76,42 +44,78 @@ const BarangayLogin = () => {
 			</div>
 
 			<div className="block max-w-lg rounded-lg bg-white p-9 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-				<Form className="flex flex-col items-center" onSubmit={handleLogin}>
-					<h2 className="mb-3">Welcome Barangay Admin</h2>
-					<Form.Group className="mb-3 w-full" controlId="formLogin">
-						<Form.Label>Email</Form.Label>
-						<Form.Control
-							type="email"
-							placeholder="Barangay authorized email"
-							aria-describedby="admin email"
-							value={email}
-							name="email"
-							onChange={handleInputChange}
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3 w-full relative">
-						<Form.Label>Password</Form.Label>
-						<Form.Control
-							type={showPassword ? "text" : "password"}
-							placeholder="Enter Password"
-							aria-describedby="admin password"
-							value={password}
-							name="password"
-							onChange={handleInputChange}
-						/>
-						<i onClick={togglePasswordVisibility}>
-							{showPassword ? (
-								<VisibilityOffRoundedIcon className="absolute cursor-pointer top-[40px] bottom-0 right-[18px]" />
-							) : (
-								<VisibilityRoundedIcon className="absolute cursor-pointer top-[40px] bottom-0 right-[18px]" />
-							)}
-						</i>
-					</Form.Group>
-					{error && <p className="text-color-re">{error}</p>}
-					<Button variant="success" size="md" type="submit">
-						Login
-					</Button>
-				</Form>
+				<Formik
+					validationSchema={schema}
+					onSubmit={async (values) => {
+						try {
+							await UserLogin(values.email, values.password);
+						} catch (error) {
+							if (error.code == "auth/user-not-found") {
+								toast.error("User not found");
+							} else if (error.code == "auth/wrong-password") {
+								toast.error("Wrong password");
+							} else {
+								toast.error(error.message);
+							}
+						}
+					}}
+					initialValues={{
+						email: "",
+						password: "",
+					}}
+				>
+					{({ handleSubmit, handleChange, values, touched, errors }) => (
+						<Form
+							noValidate
+							className="flex flex-col items-center"
+							onSubmit={handleSubmit}
+						>
+							<h2 className="mb-3">Welcome Admin</h2>
+							<Form.Group className="mb-3 w-full" controlId="formLogin">
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="CLENRO authorized email"
+									aria-describedby="admin email"
+									value={values.email}
+									isValid={touched.email && !errors.email}
+									isInvalid={!!errors.email}
+									name="email"
+									onChange={handleChange}
+								/>
+								<Form.Control.Feedback type="invalid">
+									{errors.email}
+								</Form.Control.Feedback>
+							</Form.Group>
+							<Form.Group className="mb-3 w-full relative">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type={showPassword ? "text" : "password"}
+									placeholder="Enter Password"
+									aria-describedby="admin password"
+									value={values.password}
+									isValid={touched.password && !errors.password}
+									isInvalid={!!errors.password}
+									name="password"
+									onChange={handleChange}
+								/>
+								<i onClick={togglePasswordVisibility}>
+									{showPassword ? (
+										<VisibilityOffRoundedIcon className="absolute cursor-pointer top-[40px] bottom-0 right-[35px]" />
+									) : (
+										<VisibilityRoundedIcon className="absolute cursor-pointer top-[40px] bottom-0 right-[35px]" />
+									)}
+								</i>
+								<Form.Control.Feedback type="invalid">
+									{errors.password}
+								</Form.Control.Feedback>
+							</Form.Group>
+							<Button variant="success" size="md" type="submit">
+								Login
+							</Button>
+						</Form>
+					)}
+				</Formik>
 			</div>
 		</div>
 	);
